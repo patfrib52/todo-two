4b1541e444ee920fb4b61f1771a5e6bfdf8a3896
jacques <frichot.p@gmail.com>
2026-06-21 20:02:18 +1000

    Add todo completion toggle with checkbox

--- todo.js
+++ todo.js
@@ -30,8 +30,9 @@ const addTodos = (todo) => {
   todos.push(todo);
 };
 
-const completeTodo = (targetIndex) => {
-  todos[targetIndex].completed = true;
+const todoStatus = (targetIndex, state) => {
+  todos[targetIndex].completed = state;
+  displayTodo();
 };
 
 const deleteTodo = (targetIndex) => {
@@ -42,27 +43,41 @@ const deleteTodo = (targetIndex) => {
 const displayTodo = () => {
   console.clear();
   displayList.innerHTML = "";
+
   todos.forEach((element, index) => {
+
     const container = document.createElement("div");
     const newTask = document.createElement("p");
+
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     deleteBtn.dataset.id = index;
     deleteBtn.addEventListener("click", () => {
-      deleteTodo(deleteBtn.dataset.id);
+      deleteTodo(index);
+    });
+
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.addEventListener("change", (event) => {
+      const target = event.target;
+      const state = target.checked ? true : false;
+      todoStatus(index, state);
+      
     });
 
     const status = element.completed;
-
+    
     let newStatus;
+
     if (status === false) {
       newStatus = "- Not Completed";
     } else if (status === true) {
       newStatus = "- Completed";
     }
+    checkBox.checked = element.completed;
     console.log(`${index + 1 + "."}`, element.title, newStatus);
     newTask.innerHTML = `${index + 1 + "."} ${element.title} ${newStatus}`;
     displayList.appendChild(container);
-    container.append(newTask, deleteBtn);
-  });
-};
+    container.append(newTask, checkBox, deleteBtn);
+
+  })};
